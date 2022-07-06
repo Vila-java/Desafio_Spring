c93cf01f3c75b7023624da572bed8d5b3c10307b
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-06 13:56:13 -0300

    Iniciando projeto

---
+++ src/test/java/com/spring_desafio/SpringDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
